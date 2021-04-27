551f0b0cd18a841891e70016e172e4c4fcfe6a5c
julioarranz <julioarranzmedina@gmail.com>
2021-04-27 14:25:07 +0200

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package inetum.com.maven01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
